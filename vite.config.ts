9562e20c9eeb4362fffabf650b39e3b97064df94
vinivini220 <vinivini1688@gmail.com>
2025-09-26 17:07:27 +0530

    q

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+// import BASE_URL from "meta/env";
 
 export default defineConfig(({ mode }) => ({
   server: {
@@ -11,8 +12,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./computa-renal-care/src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/computa-renal-care/",
 }));
