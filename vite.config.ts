80460fd84104d7fe8088cbc7148ddc275174d668
vinivini220 <vinivini1688@gmail.com>
2025-09-26 15:08:34 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
   },
+  // 👇 Repo name path exactly match பண்ணுங்க
   base: "/computa-renal-care/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
